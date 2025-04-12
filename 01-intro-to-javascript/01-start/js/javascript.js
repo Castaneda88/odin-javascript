4043bc8721de57dda63c29a169afc4f603bd3f6f
Michael Castaneda <178906498+Castaneda88@users.noreply.github.com>
2025-04-12 03:05:46 -0700

    add script and link script file
    adding script to index.html to follow lessons
    that include js

---
+++ 01-intro-to-javascript/01-start/js/javascript.js
@@ -0,0 +1,44 @@
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+let age = 11;
+console.log(age);
+
+age = 54;
+
+console.log(age);
+
+const pi = 3.14;
+
+console.log(pi);
+
+console.log((3 + 2) - 76 * (1 + 1))
+
+console.log(23+97)
+
+console.log((120 - 7) + 3 + 5 * (2 +2))
+
+console.log((4+6+9) / 77)
+
+let a = 10;
+
+console.log(a)
+
+a = 7;
+
+console.log(a)
+
+let b = 7 * a;
+
+console.log(b)
+
+const max = 57;
+
+const actual = max - 13;
+
+const percentage = actual / max;
+
+console.log(percentage)
